aa23f25eb5b42c823b2c6f65789d6c5ae934f08d
Jananga Hasitha <jananga1999@gmail.com>
2021-09-23 11:40:18 +0530

    CSELIBRARY:fibonacci added

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -15,4 +15,9 @@ public class MathOperation {
         return operand1 - operand2;
     }
 
+    public static long fib(long n){
+        if(n==0)    return 0;
+        if(n==1)    return 1;
+        return MathOperation.fib(n-1)+MathOperation.fib(n-2);
+    }
 }
